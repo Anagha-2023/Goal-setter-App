f1c903b616a37e828a063f560cbd4806bb018f4a
Anagha-2023 <140034415+Anagha-2023@users.noreply.github.com>
2024-06-10 19:11:20 +0530

    complete login,register,logout

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
-import { useState } from "react"
+import { useState , useEffect} from "react"
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { FaSignInAlt } from "react-icons/fa"
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/spinner'
 
 
 function Login() {
@@ -17,35 +22,61 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+    dispatch(login(userData))
   }
 
-  const {email,password} = formData
+  const { email, password } = formData
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  return <> 
-  <section className="heading">
-    <h1>
-      <FaSignInAlt/> Login
-    </h1>
-    <p>Please Login to your Account</p>
-  </section>
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth)
 
-  <section className="form">
-    <form  onSubmit={onSubmit}>
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+    if (isSuccess || user) {
+      navigate('/')
+    }
+    dispatch(reset())
 
-      <div className="form-group"> 
-      <input type="email" className="form-control" id="email" name="email" value={email} placeholder="Enter your e-mail" onChange={onChange}/>
-      </div>
+  }, [user, isError, isSuccess, message, navigate, dispatch])
 
-      <div className="form-group"> 
-      <input type="password" className="form-control" id="password" name="password" value={password} placeholder="Enter your password" onChange={onChange}/>
-      </div>
+  if(isLoading) {
+    return <Spinner/>
+  }
 
-      <div className="form-group">
-        <button type="submit" className="btn btn-block">Submit</button>
-      </div>
-    </form>
-  </section>
+  return <>
+    <section className="heading">
+      <h1>
+        <FaSignInAlt /> Login
+      </h1>
+      <p>Please Login to your Account</p>
+    </section>
+
+    <section className="form">
+      <form onSubmit={onSubmit}>
+
+        <div className="form-group">
+          <input type="email" className="form-control" id="email" name="email" value={email} placeholder="Enter your e-mail" onChange={onChange} />
+        </div>
+
+        <div className="form-group">
+          <input type="password" className="form-control" id="password" name="password" value={password} placeholder="Enter your password" onChange={onChange} />
+        </div>
+
+        <div className="form-group">
+          <button type="submit" className="btn btn-block">Submit</button>
+        </div>
+      </form>
+    </section>
   </>
 }
 
